9114bd6aab1272128f53643f969b15cb2b88c6d0
youssef <youssefahmedabdelkader25@gmail.com>
2024-03-26 04:57:48 +0200

    final

---
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+x = 122
+while x >= 97:
+    print("{:c}".format(x), end="")
+    x -= 1
---
+++ try.py
@@ -0,0 +1,5 @@
+import termcolor
+import pyfiglet
+import datetime
+#print(pyfiglet.figlet_format("# Youssef"))
+print(termcolor.colored(pyfiglet.figlet_format("# HI Youssef"), color= "green"))
