996f8e3793efdacfe4e8ceb3c63895b524dcf9bc
youssef <youssefahmedabdelkader25@gmail.com>
2024-04-06 09:55:15 +0200

    final

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import stderr
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (TypeError, ValueError) as err:
+        stderr.write("Exception: " + str(err) + '\n')
+        return (False)
