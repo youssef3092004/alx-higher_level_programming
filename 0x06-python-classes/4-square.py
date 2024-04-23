8b6ccb2420cfd870ca77c6875b8307a16a61e235
youssef <youssefahmedabdelkader25@gmail.com>
2024-04-23 22:21:06 +0200

    final

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -24,3 +24,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+    def area(self):
+        """Area of this square
+        Returns: the size squared"""
+        return self.__size ** 2
