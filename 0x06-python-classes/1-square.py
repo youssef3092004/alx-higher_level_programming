f3ae434e786935836ed5dbaffb5328a536ee5085
youssef <youssefahmedabdelkader25@gmail.com>
2024-04-23 20:43:21 +0200

    final

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Square Module"""
+
+class Square:
+    def __init__(self, size):
+        """Construct
+        size: length of a side of the square"""
+        self.__size = size
