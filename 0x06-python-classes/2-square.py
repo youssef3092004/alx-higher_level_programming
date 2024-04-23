103b781634dede878c593df68894bb21ebd7ea00
youssef <youssefahmedabdelkader25@gmail.com>
2024-04-23 21:31:19 +0200

    final

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Square Module"""
+class Square:
+    def __init__(self, size=0):
+        """Construct
+        size: length of a side of the square
+        TypeError: If size is not an integer
+        ValueError: If size in less than 0"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
