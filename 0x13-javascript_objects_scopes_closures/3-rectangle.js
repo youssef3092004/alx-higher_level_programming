2cb6e6e7913eb16921671963f23ec95d285662ec
youssef <youssefahmedabdelkader25@gmail.com>
2024-06-20 21:48:13 +0300

    final

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,6 +7,17 @@ class Rectangle {
     }
   }
 
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
   print () {
     for (let i = 0; i < this.height; i++) {
       console.log('X'.repeat(this.width));
