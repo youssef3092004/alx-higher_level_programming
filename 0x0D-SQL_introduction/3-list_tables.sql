38acf002518124769eb549298120f25f1b756a7c
youssef <youssefahmedabdelkader25@gmail.com>
2024-07-02 18:08:28 +0300

    final

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,2 +1,2 @@
 -- show all the table in database
-SELECT * FROM hbtn_0c_0;
+SHOW TABLE;
---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,5 @@
+    -- create teh table called first table and has two attribute id and name
+    CREATE TABLE first_table(
+        id INT,
+        name VARCHAR(256);
+    )
