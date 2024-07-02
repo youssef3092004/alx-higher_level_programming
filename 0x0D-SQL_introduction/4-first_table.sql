71ece879858ee6661d9384c67b7f8e65a4a97072
youssef <youssefahmedabdelkader25@gmail.com>
2024-07-02 18:09:41 +0300

    fianl

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,5 +1,5 @@
     -- create teh table called first table and has two attribute id and name
-    CREATE TABLE first_table(
+    CREATE TABLE IF NOT EXISTS first_table(
         id INT,
-        name VARCHAR(256);
-    )
+        name VARCHAR(256)
+    );
