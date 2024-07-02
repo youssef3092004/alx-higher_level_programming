3d394218e5b565a75100f217b7d51dcfcc4840f4
youssef <youssefahmedabdelkader25@gmail.com>
2024-07-02 17:51:11 +0300

    final

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 -- create database if not exist called hbtn_0c_0
-CREATE TABLE IF NOT EXISTS hbtn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,2 @@
+-- remove database if not exist called hbtn_0c_0
+DROP DATABASE IF EXISTS hbtn_0c_0;
