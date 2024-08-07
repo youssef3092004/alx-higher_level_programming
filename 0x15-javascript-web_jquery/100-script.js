1c97599bec4c7edd191c4afcc792dc5283c616ad
youssef <youssefahmedabdelkader25@gmail.com>
2024-08-08 00:03:33 +0300

    semistandard

--- 0x15-javascript-web_jquery/100-script.js
+++ 0x15-javascript-web_jquery/100-script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const header = document.querySelector('header');
-    if (header) {
-      header.style.color = '#FF0000';
-    }
-  });
+  const header = document.querySelector('header');
+  if (header) {
+    header.style.color = '#FF0000';
+  }
+});
