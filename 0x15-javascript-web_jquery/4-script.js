7db1fd37b47da34ad8c9583f66e3f1b4ec3c3619
youssef <youssefahmedabdelkader25@gmail.com>
2024-08-07 23:15:20 +0300

    final

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,8 @@
+$('DIV#toggle_header').click(function () {
+    if (($('header')).hasClass('red')){
+        $('header').removeClass('red').addClass('green');
+    }
+    else if ($('header').hasClass('green')) {
+        $('header').removeClass('green').addClass('red');
+    }
+});
